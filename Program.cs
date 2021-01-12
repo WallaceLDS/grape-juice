ddd014828618346a531636c51fd3f7bb18e1c250
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2021-01-12 03:29:46 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grapeJuice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 25% Grape Juice
+
+## 100% Orange Juice bootleg version
+
+Using C# + .Net core 3.1
