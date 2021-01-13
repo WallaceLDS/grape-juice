929979102f585980e050f2bbf8871dd046e65f4c
Wallace lima <wallacelimafs08@hotmail.com>
2021-01-13 01:20:42 -0300

    CLASS_PERSONAGEM_CREATE_ATTRIBUTES_CONSTRUCT_GET

--- Personagem.cs
+++ Personagem.cs
@@ -2,15 +2,39 @@ using System;
 
 namespace grapeJuice
 {    
+    //CLASS Personagem
     public class Personagem
     {
-        void personagem(){
-            int hp = 0;
-            int atk = 0;
-            int def = 0;
-            int evs= 0;            
+            //attributes
+            int _hp;
+            int _atk;
+            int _def;
+            int _evs;
 
-        }
+            //CONSTRUCT
+            public Personagem(){
+            _hp = 0;
+            _atk = 0;
+            _def = 0;
+            _evs= 0;
+            }
+
+            //GET
+            public int GEThp(){
+                return _hp;            
+            }
+            public int GETatk(){
+                return _atk;            
+            }
+            public int GETdef(){
+                return _def;            
+            }
+            public int GETevs(){
+                return _evs;            
+            }
+
+                
+
 
         
     }
