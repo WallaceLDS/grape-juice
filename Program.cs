31504e91c95c00649895aeb11376baf24b3f0994
Wallace lima <wallacelimafs08@hotmail.com>
2021-01-13 16:00:31 -0300

    CREATE_function_Combat

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ namespace grapeJuice
             Console.WriteLine($"Vida do personagem 1= {p1.GEThp()}");
             Console.WriteLine($"Vida do personagem 2= {p2.GEThp()}");
             Console.WriteLine($"Personagem 2 atacando Personagem 1....");
+            Console.WriteLine($"-----------MENU COMBAT--------------");
+            Console.WriteLine($"1-Atacar");
+            Console.WriteLine($"2-Defender");
+            Console.WriteLine($"3-Evasiva");
+            string[] escolha = Console.ReadLine().Split(" ");
+            int _opcao = int.Parse(escolha[0]);
+            Combat(_opcao,p2,p1);
+            Console.WriteLine($"-----------------------------------------");
             /* 
                P1   |  P2
             hp=5    |hp=3
@@ -18,10 +26,11 @@ namespace grapeJuice
             def=1   |def=1
             evs=1   |evs=2
             */
-            p1.SEThp(p2.GETatk() - p1.GETdef());
             Console.WriteLine($"Vida do personagem 1= {p1.GEThp()}");
             Console.WriteLine($"Vida do personagem 2= {p2.GEThp()}");
+            Console.WriteLine($"-----------------------------------------");
             Console.WriteLine($"Personagem 1 atacando Personagem 2....");
+
             /* 
                P1   |  P2
             hp=4   |hp=3
@@ -29,16 +38,32 @@ namespace grapeJuice
             def=1   |def=1
             evs=1   |evs=2
             */
-            p2.SEThp(p1.GETatk() - p2.GETdef());
-            Console.WriteLine($"Vida do personagem 1= {p1.GEThp()}");
-            Console.WriteLine($"Vida do personagem 2= {p2.GEThp()}");
+            //p2.SEThp(p1.GETatk() - p2.GETdef());
+            //Console.WriteLine($"Vida do personagem 1= {p1.GEThp()}");
+            //Console.WriteLine($"Vida do personagem 2= {p2.GEThp()}");
 
-        }
+             // Função do dado, returna um numero aleatorio de 1 a 6
+            private int Dado(){
+                Random rnd = new Random();
+                return rnd.Next(1,6);  
+            }
+            public int Combat(int opcao,Personagem atacante, Personagem denfensor){
+                switch (opcao)
+                {
+                    //1-ATAQUE
+                    case 1:
+                {
+                    return atacante.SEThp(atacante.GETatk()-denfensor.GETdef());   
+                }
+                    //
+                    /*case 2:
+                    {
 
-        // Função do dado, returna um numero aleatorio de 1 a 6
-        private int Dado(){
-            Random rnd = new Random();
-            return rnd.Next(1,6);  
-        }
+                    }*/                
+                    default: 
+                    return 1;
+                }    
+            }
+        }    
     }
 }
